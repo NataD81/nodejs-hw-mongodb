bb2ca362fabd114b8dc0669a7cd0907c56602f14
NataD81 <dvoryanchuk81@gmail.com>
2024-09-16 10:40:35 +0300

    up

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import { initMongoDB } from './db/initMongoDB.js';
+import { startServer } from './server.js';
+
+
+const bootstrap = async () => {
+    await initMongoDB();
+    startServer();
+};
+
+bootstrap();
+
